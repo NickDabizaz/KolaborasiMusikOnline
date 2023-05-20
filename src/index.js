d9ffb8915856361ca4e874df6dac89cd5c7fdd6e
NickDabizaz <94162428+NickDabizaz@users.noreply.github.com>
2023-05-20 14:22:53 +0700

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// ./index.js
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+const musisiRoutes = require('./routes/musisiRoutes');
+const produserRoutes = require('./routes/produserRoutes');
+
+app.use(express.json());
+
+app.use('/api/v1/musisi', musisiRoutes);
+app.use('/api/v1/produser', produserRoutes);
+app.use('/api/v1/users', userRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
